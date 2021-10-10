d5618498ae79af18bf4ba0f6bd45b611d9e7264e
Abrahamrlvas <workabrahamrlvas@gmail.com>
2021-10-10 20:08:13 +0800

    Correcion de error

--- src/app.js
+++ src/app.js
@@ -5,9 +5,8 @@ const cors = require("cors");
 const path = require("path");
 
 //Configuración
-dotenv.config();
 const app = express();
-const port = process.env.PORT || 4000;
+
 
 //Middlewares
 app.use(express.static("public"));
