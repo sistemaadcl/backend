ba7ce36c20ca708d58e535a9ecccb090f930cb6f
Abrahamrlvas <workabrahamrlvas@gmail.com>
2021-10-18 23:35:06 +0800

    Creado controlador para crear clientes

--- src/routes/clients.routes.js
+++ src/routes/clients.routes.js
@@ -1,4 +1,7 @@
 const { Router } = require('express');
 const router = Router();
+const clientController = require('../controllers/clients.controllers')
+
+router.post('/client', clientController.newClient)
 
 module.exports = router;
