55db9d8c70aca18dcfc9dd358fa41ea5dca2d046
Abrahamrlvas <workabrahamrlvas@gmail.com>
2021-10-19 01:18:02 +0800

    Creado controlador para crear cotizaciones

--- src/controllers/quotes.controllers.js
+++ src/controllers/quotes.controllers.js
@@ -1,7 +1,10 @@
 const Quotes = require('../models/client.models');
 
 module.exports = {
-  createQuote(){
-    
+  async createQuote(){
+    const { name, request } = req.body;
+    const quotes = new Quotes({ name, request});
+    const newQuotes = await quotes.save();
+    res.json(newQuotes);
   }
 }
